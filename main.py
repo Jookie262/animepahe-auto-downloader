daa751bea174ed67c1f9b2bee36227e14975a836
Jookie262 <barrientos.134038@kalibo.sti.edu.ph>
2022-10-21 18:12:58 +0800

    Type anime in search bar

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://google.com/")
+driver.get("https://animepahe.com/")
+time.sleep(2)
+
+# Search anime 
+search = driver.find_element("name", "q")
+search.send_keys("Boruto");
+time.sleep(2)
+
 
